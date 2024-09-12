ba8ec63f19b71d6674dd30d4e6d96599c458dd25
drypzz <fdfjonfmd@gmail.com>
2024-09-12 19:23:11 -0300

    feat: Updated form

--- assets/include/form.php
+++ assets/include/form.php
@@ -1,5 +1,12 @@
-<footer>
-        <p>&copy; 2024 My Website. All rights reserved.</p>
-    </footer>
-</body>
-</html>
+<form action="assets/scripts/form-handler.php" method="POST">
+    <label for="name">Name:</label>
+    <input type="text" id="name" name="name" required>
+
+    <label for="email">Email:</label>
+    <input type="email" id="email" name="email" required>
+
+    <label for="message">Message:</label>
+    <textarea id="message" name="message" required></textarea>
+
+    <button type="submit">Send</button>
+</form>
--- contact.php
+++ contact.php
@@ -3,18 +3,9 @@
 
 <div class="content">
     <h1>Contact Us</h1>
-    <form action="assets/scripts/form-handler.php" method="POST">
-        <label for="name">Name:</label>
-        <input type="text" id="name" name="name" required>
 
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required>
-
-        <label for="message">Message:</label>
-        <textarea id="message" name="message" required></textarea>
-
-        <button type="submit">Send</button>
-    </form>
+    <?php include 'assets/include/form.php'; ?>
+    
 </div>
 
 <?php include 'assets/include/footer.php'; ?>
